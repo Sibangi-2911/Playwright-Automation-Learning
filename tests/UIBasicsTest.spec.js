97666bb8a3b50c07c0e9a50238e44811b9347c3a
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-02-22 18:12:48 +0530

    setup assertion of playwright test for title is checked in chromium browser

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,3 +1,15 @@
-const test = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
-test("First Playwright test", async () => {});
+test("Browser Context Playwright test", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  console.log(page.title());
+});
+
+test.only("Page Playwright test", async ({ page }) => {
+  await page.goto("https://google.com");
+  //get title - assertion
+  console.log(await page.title());
+  expect(page).toHaveTitle("Google");
+});
