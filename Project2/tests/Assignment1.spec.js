5b8b52282756b724e58610f0295231695c58f0a3
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-02-23 12:19:04 +0530

    successfully the titles are displayed and wait mechanism implemented for service based applications

--- Project2/tests/Assignment1.spec.js
+++ Project2/tests/Assignment1.spec.js
@@ -3,6 +3,17 @@ const { test, expect } = require("@playwright/test");
 test.only("Login Test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
+  const cardTitle = page.locator(".card-body b");
   await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
   console.log(await page.title());
+  await expect(page).toHaveTitle("Let's Shop");
+
+  await page.locator("#userEmail").fill("sibangiboxipatro@gmail.com");
+  await page.locator("#userPassword").fill("Sibangi@123");
+  await page.locator("#login").click();
+
+  //wait mechanism in service based applications
+  await page.waitForLoadState("networkidle");
+  const allTitles = await cardTitle.allTextContents();
+  console.log(allTitles);
 });
