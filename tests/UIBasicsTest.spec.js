d429f46b4be894d80bb226727bdab815f4b4bcde
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-02-23 11:10:24 +0530

    used locators supported by playwright, extracting text from browser, inserting valid expect assertion & extracting multiple web elements in a page

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,13 +1,30 @@
 const { test, expect } = require("@playwright/test");
 
-test("Browser Context Playwright test", async ({ browser }) => {
+test.only("Browser Context Playwright test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
+  const userName = page.locator("#username");
+  const signIn = page.locator("#signInBtn");
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   console.log(page.title());
+
+  //css selector for identifying edit box
+  await userName.fill("rahulshetty");
+  await page.locator("[type = 'password']").fill("Learning@830$3mK2");
+  await signIn.click();
+  console.log(await page.locator("[style *= 'block']").textContent());
+  await expect(page.locator("[style *= 'block']")).toContainText("Incorrect");
+
+  //enter valid credentials and erase existing ones
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy");
+  await signIn.click();
+  console.log(await page.locator(".card-body a").first().textContent());
+  console.log(await page.locator(".card-body a").nth(1).textContent());
+  console.log(await page.locator(".card-body a").last().textContent());
 });
 
-test.only("Page Playwright test", async ({ page }) => {
+test("Page Playwright test", async ({ page }) => {
   await page.goto("https://google.com");
   //get title - assertion
   console.log(await page.title());
