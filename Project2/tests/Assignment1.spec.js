aa10322290c6061e7364a7efbf4ed9bcfdc459db
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-02-24 21:47:19 +0530

    dynamically found the order from orderHistory page and completed the automation testing

--- Project2/tests/Assignment1.spec.js
+++ Project2/tests/Assignment1.spec.js
@@ -91,6 +91,25 @@ test.only("Login Test", async ({ browser }) => {
     .locator(".em-spacer-1 .ng-star-inserted")
     .textContent();
   console.log(orderId);
+
+  //opening orders page
+  await page.locator("button[routerlink*='/dashboard/myorders']").click();
+  await page.locator("tbody").waitFor();
+
+  //find order
+  const rows = await page.locator("tbody tr");
+  for (let i = 0; i < (await rows.count()); i++) {
+    const rowOrderId = await rows.nth(i).locator("th").textContent();
+    if (orderId.includes(rowOrderId)) {
+      await rows.nth(i).locator("button").first().click();
+      break;
+    }
+  }
+
+  //after view button is clicked
+  const orderIdDetails = await page.locator(".col-text").textContent();
+  expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
   await page.pause();
 });
 
