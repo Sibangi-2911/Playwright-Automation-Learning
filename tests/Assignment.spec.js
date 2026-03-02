d3abe208b1393cdf951e3007229cdd8e2e636292
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-02 22:32:48 +0530

    Login using getByLabel() and getByRole()

--- tests/Assignment.spec.js
+++ tests/Assignment.spec.js
@@ -7,4 +7,8 @@ test("End-to-End E-Commerce Flow", async (page) => {
   await page.getByLabel("Email").fill("sibangiboxipatro@gmail.com");
   await page.getByLabel("Password").fill("Sibangi@123");
   await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+
+  //Dynamically select a product and add to cart
 });
