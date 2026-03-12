0e6a34895a9909eee3166d9212fd091ad89d85c4
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-12 09:45:46 +0530

    Validated if element is hidden, displayed mode with Expect assertions

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -8,4 +8,9 @@ test("Popup validations", async ({ page }) => {
   await page.goBack(); //go to rahulshetty
   await page.goForward(); //go to google again
   await page.goBack();
+
+  // Validate if element is hidden, displayed mode with Expect assertions
+  await expect(page.locator("#displayed-text")).toBeVisible();
+  await page.locator("#hide-textbox").click();
+  await expect(page.locator("#displayed-text")).toBeHidden();
 });
