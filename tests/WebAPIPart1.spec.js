7111e3cfa97a2fbf3f25d1b7daf8cc8c385027c2
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-13 08:45:00 +0530

    API & Web Integration for login API done

---
+++ tests/WebAPIPart1.spec.js
@@ -0,0 +1,126 @@
+const { test, expect, request } = require("@playwright/test");
+const loginPayLoad = {
+  userEmail: "sibangiboxipatro@gmail.com",
+  userPassword: "Sibangi@123",
+};
+let token;
+
+test.beforeAll(async () => {
+  //Login API
+  const apiContext = await request.newContext();
+  const loginResponse = await apiContext.post(
+    "https://rahulshettyacademy.com/api/ecom/auth/login",
+    { data: loginPayLoad },
+  );
+  expect(loginResponse.ok()).toBeTruthy(); // it will check whether the response is success??
+  const loginResponseJson = await loginResponse.json();
+  token = loginResponseJson.token;
+  console.log(token);
+});
+
+test.only("Place the order", async ({ browser }) => {
+  const context = await browser.newContext();
+  //insert javascript inside this to add cookies to storage
+  await context.addInitScript((value) => {
+    window.localStorage.setItem("token", value);
+  }, token);
+
+  const page = await context.newPage();
+
+  const productName = "ZARA COAT 3";
+
+  await page.goto("https://rahulshettyacademy.com/client/");
+
+  await expect(page).toHaveTitle("Let's Shop");
+  //wait mechanism in service based applications
+  const cardTitle = page.locator(".card-body b");
+  await page.locator(".card-body b").first().waitFor();
+  const allTitles = await cardTitle.allTextContents();
+  console.log(allTitles);
+
+  //end to end automation testing
+  await page
+    .locator(".card-body")
+    .filter({ hasText: productName })
+    .getByRole("button", { name: "Add To Cart" })
+    .click();
+
+  await page
+    .getByRole("listitem")
+    .getByRole("button", { name: "Cart" })
+    .click();
+
+  await page.locator("div li").first().waitFor(); //becoz isVisible doesn't support auto wait
+  await expect(page.getByText(productName)).toBeVisible();
+
+  await page.getByRole("button", { name: "Checkout" }).click();
+
+  //credit card field
+  await page
+    .locator(".field:has-text('Credit Card Number') input")
+    .fill("4542993192922293");
+
+  //cvv code
+  await page.locator(".field:has-text('CVV Code') input").fill("204");
+
+  //Name on card
+  await page
+    .locator(".field:has-text('Name on Card') input")
+    .fill("SIBANGI BOXIPATRO");
+
+  //Apply Coupon
+  await page.locator("[name*='coupon']").fill("rahulshettyacademy");
+  await page.locator("[type = 'submit']").click();
+
+  //Expiry date dropdown
+  const expiryDropdowns = page.locator(".field:has-text('Expiry Date') select");
+  await expiryDropdowns.first().selectOption("03");
+  await expiryDropdowns.nth(1).selectOption("27");
+
+  //Shipping Information suggestive dropdown
+  const country = page.getByPlaceholder("Select Country");
+  await country.click();
+  await country.pressSequentially("ind", { delay: 150 });
+
+  await page.getByRole("button", { name: "India" }).nth(1).click();
+
+  //Shipping Information email
+  await expect(page.locator(".user__name [type='text']").first()).toHaveText(
+    "sibangiboxipatro@gmail.com",
+  );
+
+  //place order button
+  await page.getByText("Place Order").click();
+
+  //order confirmation page
+  await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+
+  //grab order id
+  const orderId = await page
+    .locator(".em-spacer-1 .ng-star-inserted")
+    .textContent();
+  console.log(orderId);
+
+  //opening orders page
+  await page.locator("button[routerlink*='/dashboard/myorders']").click();
+  await page.locator("tbody").waitFor();
+
+  //find order
+  const rows = await page.locator("tbody tr");
+  for (let i = 0; i < (await rows.count()); i++) {
+    const rowOrderId = await rows.nth(i).locator("th").textContent();
+    if (orderId.includes(rowOrderId)) {
+      await rows.nth(i).locator("button").first().click();
+      break;
+    }
+  }
+
+  //after view button is clicked
+  const orderIdDetails = await page.locator(".col-text").textContent();
+  expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
+  await page.pause();
+});
+
+// Verify if order created is showing in history page
+// precondition - create order
