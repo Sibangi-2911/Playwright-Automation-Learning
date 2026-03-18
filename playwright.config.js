4aff3d864d819fa6fafadddea77b59c6657cfa32
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-18 12:17:24 +0530

    Modified the timeout

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,7 @@ import { defineConfig, devices } from "@playwright/test";
  */
 const config = {
   testDir: "./tests",
-  timeout: 100 * 1000,
+  timeout: 40 * 1000,
   launchOptions: {
     slowMo: 800,
   },
