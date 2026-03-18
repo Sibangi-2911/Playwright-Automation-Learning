6f45659e46e1038cdd66e54a4b46d2d3a140a357
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-18 10:24:04 +0530

    saved session storage and injected into new browser context

---
+++ tests/WebAPIPart2.spec.js
@@ -0,0 +1,137 @@
+//This is to be done when the login is complex
+//Loging UI ----- .json file will store all the storage data while logged in
+//test browser(inject json directly inside browser), cart,order,orderdetails, orderhistory
+
+const { test, expect } = require("@playwright/test");
+let webContext;
+
+test.beforeAll(async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const email = "sibangiboxipatro@gmail.com";
+  await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+  await page.getByPlaceholder("email@example.com").fill(email);
+  await page.getByPlaceholder("enter your passsword").fill("Sibangi@123");
+  await page.getByRole("button", { name: "login" }).click();
+  //wait mechanism in service based applications
+  await page.waitForLoadState("networkidle");
+
+  //save session storage
+  await context.storageState({ path: "state.json" });
+
+  //this time by creating a new context we are injecting the storage data
+  webContext = await browser.newContext({ storageState: "state.json" });
+});
+
+test("Login Test", async () => {
+  const email = "sibangiboxipatro@gmail.com";
+  const page = await webContext.newPage();
+  await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+  const products = page.locator(".card-body");
+  const productName = "ZARA COAT 3";
+  const cardTitle = page.locator(".card-body b");
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Let's Shop");
+
+  await page.locator(".card-body b").first().waitFor();
+  const allTitles = await cardTitle.allTextContents();
+  console.log(allTitles);
+
+  //end to end automation testing
+  await page
+    .locator(".card-body")
+    .filter({ hasText: productName })
+    .getByRole("button", { name: "Add To Cart" })
+    .click();
+
+  await page
+    .getByRole("listitem")
+    .getByRole("button", { name: "Cart" })
+    .click();
+
+  await page.locator("div li").first().waitFor(); //becoz isVisible doesn't support auto wait
+  await expect(page.getByText(productName)).toBeVisible();
+
+  await page.getByRole("button", { name: "Checkout" }).click();
+
+  //credit card field
+  await page
+    .locator(".field:has-text('Credit Card Number') input")
+    .fill("4542993192922293");
+
+  //cvv code
+  await page.locator(".field:has-text('CVV Code') input").fill("204");
+
+  //Name on card
+  await page
+    .locator(".field:has-text('Name on Card') input")
+    .fill("SIBANGI BOXIPATRO");
+
+  //Apply Coupon
+  await page.locator("[name*='coupon']").fill("rahulshettyacademy");
+  await page.locator("[type = 'submit']").click();
+
+  //Expiry date dropdown
+  const expiryDropdowns = page.locator(".field:has-text('Expiry Date') select");
+  await expiryDropdowns.first().selectOption("03");
+  await expiryDropdowns.nth(1).selectOption("27");
+
+  //Shipping Information suggestive dropdown
+  const country = page.getByPlaceholder("Select Country");
+  await country.click();
+  await country.pressSequentially("ind", { delay: 150 });
+
+  await page.getByRole("button", { name: "India" }).nth(1).click();
+
+  //Shipping Information email
+  await expect(page.locator(".user__name [type='text']").first()).toHaveText(
+    email,
+  );
+
+  //place order button
+  await page.getByText("Place Order").click();
+
+  //order confirmation page
+  await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+
+  //grab order id
+  const orderId = await page
+    .locator(".em-spacer-1 .ng-star-inserted")
+    .textContent();
+  console.log(orderId);
+
+  //opening orders page
+  await page.locator("button[routerlink*='/dashboard/myorders']").click();
+  await page.locator("tbody").waitFor();
+
+  //find order
+  const rows = await page.locator("tbody tr");
+  for (let i = 0; i < (await rows.count()); i++) {
+    const rowOrderId = await rows.nth(i).locator("th").textContent();
+    if (orderId.includes(rowOrderId)) {
+      await rows.nth(i).locator("button").first().click();
+      break;
+    }
+  }
+
+  //after view button is clicked
+  const orderIdDetails = await page.locator(".col-text").textContent();
+  expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
+  await page.pause();
+});
+
+//Test case 2
+test("Test case 2", async () => {
+  const page = await webContext.newPage();
+  await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+  const products = page.locator(".card-body");
+  const productName = "ZARA COAT 3";
+  const cardTitle = page.locator(".card-body b");
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Let's Shop");
+
+  await page.locator(".card-body b").first().waitFor();
+  const allTitles = await cardTitle.allTextContents();
+  console.log(allTitles);
+});
