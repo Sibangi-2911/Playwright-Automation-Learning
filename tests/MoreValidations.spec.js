df0ee518a68f3771ce0e5127c40d067f73e21065
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-23 08:02:28 +0530

    Captured  Screenshots with Playwright on page & partial Element level

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -28,3 +28,18 @@ test("Popup validations", async ({ page }) => {
   const textCheck = await framesPage.locator(".text h2").textContent();
   console.log(textCheck.split(" ")[1]);
 });
+
+//Screenshot & Visual Testing
+test.only("Screenshot & Visual comparison", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+  await page.waitForLoadState();
+
+  // Validate if element is hidden, displayed mode with Expect assertions
+  await expect(page.locator("#displayed-text")).toBeVisible();
+  await page
+    .locator("#displayed-text")
+    .screenshot({ path: "partialscreenshot.png" }); // Screenshot of a particular element
+  await page.locator("#hide-textbox").click();
+  await page.screenshot({ path: "screenshot.png" }); //Complete page screenshot
+  await expect(page.locator("#displayed-text")).toBeHidden();
+});
