8f32cee5fd888d355950ac6b834ce91c639578b9
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-24 11:56:26 +0530

    Built util functions to read and update execl file

--- ExcelJSUtil/exceldemo2.js
+++ ExcelJSUtil/exceldemo2.js
@@ -2,6 +2,7 @@ const ExcelJS = require("exceljs");
 
 //wrapped the code in asynchronous function
 async function excelTest() {
+  let output = { row: -1, column: -1 };
   const workbook = new ExcelJS.Workbook();
   await workbook.xlsx.readFile(
     "C:/Users/Sibangi Boxipatro/Downloads/exceldownloadTest.xlsx",
@@ -12,11 +13,19 @@ async function excelTest() {
       //console.log(cell.value);
 
       //To find the desired text in the excel sheet
-      if (cell.value === "Apple") {
+      if (cell.value === "Banana") {
+        output.row = rowNumber;
+        output.column = colNumber;
         console.log(rowNumber);
         console.log(colNumber);
       }
     });
   });
+  //Access the particular cell and update the text
+  const cell = worksheet.getCell(output.row, output.column);
+  cell.value = "Republic";
+  await workbook.xlsx.writeFile(
+    "C:/Users/Sibangi Boxipatro/Downloads/exceldownloadTest.xlsx",
+  );
 }
 excelTest();
