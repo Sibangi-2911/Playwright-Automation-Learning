3e97c5d195e5349a26d1d46af822eba549819804
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-25 12:53:47 +0530

    Strategy to handle download & uploading files using playwright

---
+++ tests/upload-download.spec.js
@@ -0,0 +1,52 @@
+// End to End Example of downloading file--->updating file----->uploading file with playwright test
+const { test, expect } = require("@playwright/test");
+const ExcelJS = require("exceljs");
+
+async function writeExcelTest(searchText, replaceText, change, filePath) {
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.readFile(filePath);
+  const worksheet = workbook.getWorksheet("Sheet1");
+  const output = await readExcel(worksheet, searchText);
+  const cell = worksheet.getCell(output.row, output.column + change.colChange);
+  cell.value = replaceText;
+  await workbook.xlsx.writeFile(filePath);
+}
+async function readExcel(worksheet, searchText) {
+  let output = { row: -1, column: -1 };
+  worksheet.eachRow((row, rowNumber) => {
+    row.eachCell((cell, colNumber) => {
+      //console.log(cell.value);
+
+      //To find the desired text in the excel sheet
+      if (cell.value === searchText) {
+        output.row = rowNumber;
+        output.column = colNumber;
+        console.log(rowNumber);
+        console.log(colNumber);
+      }
+    });
+  });
+  return output;
+}
+
+test("upload download excel validation", async ({ page }) => {
+  await page.goto(
+    "https://rahulshettyacademy.com/upload-download-test/index.html",
+  );
+  const downloadPromise = page.waitForEvent("download");
+  await page.getByRole("button", { name: "Download" }).click();
+  //const download =
+  await downloadPromise;
+  // const filePath = "C:/Users/Sibangi Boxipatro/Downloads/download.xlsx";
+  // await download.saveAs(filePath);
+  await writeExcelTest(
+    "Mango",
+    350,
+    { rowChange: 0, colChange: 2 },
+    "C:/Users/Sibangi Boxipatro/Downloads/download.xlsx",
+  );
+  await page.locator("#fileinput").click();
+  await page
+    .locator("#fileinput")
+    .setInputFiles("C:/Users/Sibangi Boxipatro/Downloads/download.xlsx");
+});
