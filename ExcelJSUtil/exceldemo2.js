797e6a34023d3b770f278256f3b83a0caf23d80e
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-25 10:42:03 +0530

    Get & update the data from excel based on filter search criteria done

--- ExcelJSUtil/exceldemo2.js
+++ ExcelJSUtil/exceldemo2.js
@@ -1,19 +1,24 @@
 const ExcelJS = require("exceljs");
 
 //wrapped the code in asynchronous function
-async function excelTest() {
-  let output = { row: -1, column: -1 };
+async function writeExcelTest(searchText, replacedText, change, filePath) {
   const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.readFile(
-    "C:/Users/Sibangi Boxipatro/Downloads/exceldownloadTest.xlsx",
-  );
+  await workbook.xlsx.readFile(filePath);
   const worksheet = workbook.getWorksheet("Sheet1");
+  const output = await readExcel(worksheet, searchText);
+  //Access the particular cell and update the text
+  const cell = worksheet.getCell(output.row, output.column + change.colChange);
+  cell.value = replacedText;
+  await workbook.xlsx.writeFile(filePath);
+}
+async function readExcel(worksheet, searchText) {
+  let output = { row: -1, column: -1 };
   worksheet.eachRow((row, rowNumber) => {
     row.eachCell((cell, colNumber) => {
       //console.log(cell.value);
 
       //To find the desired text in the excel sheet
-      if (cell.value === "Banana") {
+      if (cell.value === searchText) {
         output.row = rowNumber;
         output.column = colNumber;
         console.log(rowNumber);
@@ -21,11 +26,20 @@ async function excelTest() {
       }
     });
   });
-  //Access the particular cell and update the text
-  const cell = worksheet.getCell(output.row, output.column);
-  cell.value = "Republic";
-  await workbook.xlsx.writeFile(
-    "C:/Users/Sibangi Boxipatro/Downloads/exceldownloadTest.xlsx",
-  );
+  return output;
 }
-excelTest();
+//update mango price to 250
+// writeExcelTest(
+//   "Mango",
+//   "Rabbit",
+//   { rowChange: 0, colChange: 2 }, //object as a parameter to change the price
+//   "C:/Users/Sibangi Boxipatro/Downloads/exceldownloadTest.xlsx",
+// );
+
+//update mango price to 250
+writeExcelTest(
+  "Rabbit",
+  350,
+  { rowChange: 0, colChange: 2 }, //object as a parameter to change the price
+  "C:/Users/Sibangi Boxipatro/Downloads/exceldownloadTest.xlsx",
+);
