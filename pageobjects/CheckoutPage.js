61aff935e91875bd8daea74a04af443c70157948
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-27 14:44:32 +0530

    Added pageobjects folder with CheckoutPage

---
+++ pageobjects/CheckoutPage.js
@@ -0,0 +1,51 @@
+class CheckoutPage {
+  constructor(page) {
+    this.page = page;
+    this.expiryDropdowns = page.locator(
+      ".field:has-text('Expiry Date') select",
+    );
+  }
+  async fillCardDetails() {
+    //credit card field
+    await this.page
+      .locator(".field:has-text('Credit Card Number') input")
+      .fill("4542993192922293");
+
+    //cvv code
+    await this.page.locator(".field:has-text('CVV Code') input").fill("204");
+
+    //Name on card
+    await this.page
+      .locator(".field:has-text('Name on Card') input")
+      .fill("SIBANGI BOXIPATRO");
+  }
+
+  async applyCoupon(code) {
+    await this.page.locator("[name*='coupon']").fill(code);
+    await this.page.locator("[type = 'submit']").click();
+  }
+
+  async selectExpiry(month, date) {
+    //Expiry date dropdown
+    await this.expiryDropdowns.first().selectOption(month);
+    await this.expiryDropdowns.nth(1).selectOption(date);
+  }
+
+  async selectCountry() {
+    //Shipping Information suggestive dropdown
+    const country = this.page.getByPlaceholder("Select Country");
+    await country.click();
+    await country.pressSequentially("ind", { delay: 150 });
+
+    await this.page.getByRole("button", { name: "India" }).nth(1).click();
+  }
+
+  getUserEmailField() {
+    return this.page.locator(".user__name").locator("input[type='text']");
+  }
+
+  async placeOrder() {
+    await this.page.getByText("Place Order").click();
+  }
+}
+module.exports = { CheckoutPage };
