8b78320b22ca2ba9aef0419d4fe9b521ed99b7e9
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-30 10:48:00 +0530

    Implemented parameterization in running tests with different datasets

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -6,49 +6,52 @@ const dataset = JSON.parse(
   JSON.stringify(require("../Utils/placeOrderTestData.json")),
 );
 
-test.only("Login Test", async ({ browser }) => {
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  const poManager = new POManager(page);
+//Parameterization in running tests
+for (const data of dataset) {
+  test.only(`Login Test for ${data.productName}`, async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const poManager = new POManager(page);
 
-  const loginPage = poManager.getLoginPage();
-  await loginPage.goTo();
-  await loginPage.validLogin(dataset.username, dataset.password);
+    const loginPage = poManager.getLoginPage();
+    await loginPage.goTo();
+    await loginPage.validLogin(data.username, data.password);
 
-  await expect(page).toHaveTitle("Let's Shop");
-  const dashboardPage = poManager.getDashboardPage();
-  await dashboardPage.addProductToCart(dataset.productName);
-  await dashboardPage.navigateToCart();
+    await expect(page).toHaveTitle("Let's Shop");
+    const dashboardPage = poManager.getDashboardPage();
+    await dashboardPage.addProductToCart(data.productName);
+    await dashboardPage.navigateToCart();
 
-  const cartPage = poManager.getCartPage();
-  await cartPage.verifyProductInCart(dataset.productName);
-  await cartPage.proceedToCheckout();
+    const cartPage = poManager.getCartPage();
+    await cartPage.verifyProductInCart(data.productName);
+    await cartPage.proceedToCheckout();
 
-  const checkoutPage = poManager.getCheckoutPage();
-  await checkoutPage.fillCardDetails();
-  await checkoutPage.applyCoupon("rahulshettyacademy");
-  await checkoutPage.selectExpiry("03", "27");
-  await checkoutPage.selectCountry();
-  await expect(checkoutPage.getUserEmailField()).toHaveValue(dataset.username);
-  await checkoutPage.placeOrder();
+    const checkoutPage = poManager.getCheckoutPage();
+    await checkoutPage.fillCardDetails();
+    await checkoutPage.applyCoupon("rahulshettyacademy");
+    await checkoutPage.selectExpiry("03", "27");
+    await checkoutPage.selectCountry();
+    await expect(checkoutPage.getUserEmailField()).toHaveValue(data.username);
+    await checkoutPage.placeOrder();
 
-  //order confirmation page
-  const orderConfirmation = poManager.getOrderConfirmationPage();
-  await expect(orderConfirmation.verifyOrderConfirmation()).toBeVisible();
+    //order confirmation page
+    const orderConfirmation = poManager.getOrderConfirmationPage();
+    await expect(orderConfirmation.verifyOrderConfirmation()).toBeVisible();
 
-  //grab order id
-  const orderId = await orderConfirmation.getOrderId();
-  console.log("Order ID: ", orderId);
+    //grab order id
+    const orderId = await orderConfirmation.getOrderId();
+    console.log("Order ID: ", orderId);
 
-  //opening orders page
-  const ordersPage = poManager.getOrdersPage();
-  await ordersPage.goToOrdersPage();
-  await ordersPage.selectOrderById(orderId);
+    //opening orders page
+    const ordersPage = poManager.getOrdersPage();
+    await ordersPage.goToOrdersPage();
+    await ordersPage.selectOrderById(orderId);
 
-  const orderIdDetails = await ordersPage.getOrderIdDetails();
-  expect(orderId.includes(orderIdDetails)).toBeTruthy();
-  await page.pause();
-});
+    const orderIdDetails = await ordersPage.getOrderIdDetails();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    await page.pause();
+  });
+}
 
 test("Page Playwright test", async ({ page }) => {
   await page.goto("https://google.com");
