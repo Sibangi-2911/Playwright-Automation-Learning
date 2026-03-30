5e21662a3648ac0b6c2d5a47194e3897f39a3f9e
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-30 10:35:18 +0530

    Data driven from external files

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -1,26 +1,27 @@
 //End to end page object test
 const { test, expect } = require("@playwright/test");
 const { POManager } = require("../pageobjects/POManager");
+//json-->string-->js object (conversion for  testdata)
+const dataset = JSON.parse(
+  JSON.stringify(require("../Utils/placeOrderTestData.json")),
+);
 
 test.only("Login Test", async ({ browser }) => {
-  const username = "sibangiboxipatro@gmail.com";
-  const password = "Sibangi@123";
   const context = await browser.newContext();
   const page = await context.newPage();
   const poManager = new POManager(page);
 
   const loginPage = poManager.getLoginPage();
   await loginPage.goTo();
-  await loginPage.validLogin(username, password);
+  await loginPage.validLogin(dataset.username, dataset.password);
 
-  const productName = "ZARA COAT 3";
   await expect(page).toHaveTitle("Let's Shop");
   const dashboardPage = poManager.getDashboardPage();
-  await dashboardPage.addProductToCart(productName);
+  await dashboardPage.addProductToCart(dataset.productName);
   await dashboardPage.navigateToCart();
 
   const cartPage = poManager.getCartPage();
-  await cartPage.verifyProductInCart(productName);
+  await cartPage.verifyProductInCart(dataset.productName);
   await cartPage.proceedToCheckout();
 
   const checkoutPage = poManager.getCheckoutPage();
@@ -28,7 +29,7 @@ test.only("Login Test", async ({ browser }) => {
   await checkoutPage.applyCoupon("rahulshettyacademy");
   await checkoutPage.selectExpiry("03", "27");
   await checkoutPage.selectCountry();
-  await expect(checkoutPage.getUserEmailField()).toHaveValue(username);
+  await expect(checkoutPage.getUserEmailField()).toHaveValue(dataset.username);
   await checkoutPage.placeOrder();
 
   //order confirmation page
