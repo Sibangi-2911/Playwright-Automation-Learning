f394e4f8d33a23088f1d4093e7f466f1519812c8
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-04-16 14:51:12 +0530

    js file that is the compiled form of ts file

---
+++ tests/demo1.js
@@ -0,0 +1,18 @@
+"use strict";
+let message1 = "Hello";
+message1 = "Bye";
+console.log(message1);
+let age1 = 20;
+let isActive = false;
+console.log(age1);
+let numbers1 = [1, 2, 3];
+console.log(numbers1);
+//if not sure of datatype use any
+let data = "This could be anything";
+data = 42;
+console.log(data);
+console.log(typeof data);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(3, 4));
